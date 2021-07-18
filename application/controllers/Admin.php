82a76477e1fbd52262920cc8613e6b48fed63f67
Gary <zhaoyue@dlhzxt.cn>
2021-07-18 18:04:20 +0800

    版本完善

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,71 +9,81 @@
  */
 class Admin extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        if (!isset($_SESSION['user_name'])) {
-            header("Location:" . RUN . '/login/logout');
-        }
-        header("Content-type:text/html;charset=utf-8");
+	public function __construct()
+	{
+		parent::__construct();
+		if (!isset($_SESSION['user_name'])) {
+			header("Location:" . RUN . '/login/logout');
+		}
+		header("Content-type:text/html;charset=utf-8");
 		$this->load->model('Users_model', 'users');
 		$this->load->model('Role_model', 'role');
-    }
-    /**
-     * 后台首页
-     */
-    public function index()
-    {
+	}
+	/**
+	 * 后台首页
+	 */
+	public function index()
+	{
 		if (empty($_SESSION['user_name'])) {
 			echo json_encode(array('error' => false, 'msg' => "数据丢失，请重新登录。"));
 			return;
 		}
 		$rid = $_SESSION['rid'];
-		$data['role_status1'] = empty($this->role->getroleByIdRtom($rid,1))?0:1;
-		$data['role_status2'] = empty($this->role->getroleByIdRtom($rid,2))?0:1;
-		$data['role_status3'] = empty($this->role->getroleByIdRtom($rid,3))?0:1;
-		$data['role_status4'] = empty($this->role->getroleByIdRtom($rid,4))?0:1;
-		$data['role_status5'] = empty($this->role->getroleByIdRtom($rid,5))?0:1;
-		$data['role_status6'] = empty($this->role->getroleByIdRtom($rid,6))?0:1;
-		$data['role_status7'] = empty($this->role->getroleByIdRtom($rid,7))?0:1;
-		$data['role_status8'] = empty($this->role->getroleByIdRtom($rid,8))?0:1;
-        $this->display("index",$data);
-    }
-    /**
-     * 修改密码页
-     */
-    public function passwordedit()
-    {
-        $this->display("passwordedit");
-    }
-    /**
-     * 修改密码提交
-     */
-    public function userpassportsave()
-    {
-        if (empty($_SESSION['user_name'])) {
-            echo json_encode(array('error' => false, 'msg' => "无法修改密码"));
-            return;
-        }
-        $user_name = $_SESSION['user_name'];
+		/*
+				$data['role_status1'] = empty($this->role->getroleByIdRtom($rid,1))?0:1;
+				$data['role_status2'] = empty($this->role->getroleByIdRtom($rid,2))?0:1;
+				$data['role_status3'] = empty($this->role->getroleByIdRtom($rid,3))?0:1;
+				$data['role_status4'] = empty($this->role->getroleByIdRtom($rid,4))?0:1;
+				$data['role_status5'] = empty($this->role->getroleByIdRtom($rid,5))?0:1;
+				$data['role_status6'] = empty($this->role->getroleByIdRtom($rid,6))?0:1;
+				$data['role_status7'] = empty($this->role->getroleByIdRtom($rid,7))?0:1;
+				$data['role_status8'] = empty($this->role->getroleByIdRtom($rid,8))?0:1;
+		*/
 
-        $passold = isset($_POST["passold"]) ? $_POST["passold"] : '';
-        $pass = isset($_POST["pass"]) ? $_POST["pass"] : '';
+		$rid_result1 = $this->role->getroleByIdRtom($rid,1);
+		$data['role_status1'] = empty($rid_result1)?0:1;
+		$rid_result2 = $this->role->getroleByIdRtom($rid,2);
+		$data['role_status2'] = empty($rid_result2)?0:1;
+		$rid_result3 = $this->role->getroleByIdRtom($rid,3);
+		$data['role_status3'] = empty($rid_result3)?0:1;
 
-        $rest = $this->users->getUserById($user_name, $passold);
-        if (empty($rest)) {
-            echo json_encode(array('error' => false, 'msg' => "你输入的旧密码不匹配。"));
-            return;
-        }
+		$this->display("index",$data);
+	}
+	/**
+	 * 修改密码页
+	 */
+	public function passwordedit()
+	{
+		$this->display("passwordedit");
+	}
+	/**
+	 * 修改密码提交
+	 */
+	public function userpassportsave()
+	{
+		if (empty($_SESSION['user_name'])) {
+			echo json_encode(array('error' => false, 'msg' => "无法修改密码"));
+			return;
+		}
+		$user_name = $_SESSION['user_name'];
 
-        $result = $this->users->userpassportsave($user_name, $pass);
+		$passold = isset($_POST["passold"]) ? $_POST["passold"] : '';
+		$pass = isset($_POST["pass"]) ? $_POST["pass"] : '';
 
-        if ($result) {
-            echo json_encode(array('success' => true, 'msg' => "操作成功。"));
-            return;
-        } else {
-            echo json_encode(array('error' => false, 'msg' => "操作失败"));
-            return;
-        }
-    }
+		$rest = $this->users->getUserById($user_name, $passold);
+		if (empty($rest)) {
+			echo json_encode(array('error' => false, 'msg' => "你输入的旧密码不匹配。"));
+			return;
+		}
+
+		$result = $this->users->userpassportsave($user_name, $pass);
+
+		if ($result) {
+			echo json_encode(array('success' => true, 'msg' => "操作成功。"));
+			return;
+		} else {
+			echo json_encode(array('error' => false, 'msg' => "操作失败"));
+			return;
+		}
+	}
 }
--- application/controllers/Role.php
+++ application/controllers/Role.php
@@ -9,54 +9,54 @@
  */
 class Role extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        if (!isset($_SESSION['user_name'])) {
-            header("Location:" . RUN . '/login/logout');
-        }
-        $this->load->model('Users_model', 'users');
-        $this->load->model('Role_model', 'role');
-        header("Content-type:text/html;charset=utf-8");
-    }
-    /**
-     * 角色列表页
-     */
-    public function role_list()
-    {
+	public function __construct()
+	{
+		parent::__construct();
+		if (!isset($_SESSION['user_name'])) {
+			header("Location:" . RUN . '/login/logout');
+		}
+		$this->load->model('Users_model', 'users');
+		$this->load->model('Role_model', 'role');
+		header("Content-type:text/html;charset=utf-8");
+	}
+	/**
+	 * 角色列表页
+	 */
+	public function role_list()
+	{
 
-        $page = isset($_GET["page"]) ? $_GET["page"] : 1;
-        $allpage = $this->role->getroleAllPage();
-        $page = $allpage > $page ? $page : $allpage;
-        $data["pagehtml"] = $this->getpage($page, $allpage, $_GET);
-        $data["page"] = $page;
-        $data["allpage"] = $allpage;
-        $data["list"] = $this->role->getroleAllNew($page);
+		$page = isset($_GET["page"]) ? $_GET["page"] : 1;
+		$allpage = $this->role->getroleAllPage();
+		$page = $allpage > $page ? $page : $allpage;
+		$data["pagehtml"] = $this->getpage($page, $allpage, $_GET);
+		$data["page"] = $page;
+		$data["allpage"] = $allpage;
+		$data["list"] = $this->role->getroleAllNew($page);
 
-        $this->display("role/role_list", $data);
-    }
-    /**
-     * 角色添加页
-     */
-    public function role_add()
-    {
+		$this->display("role/role_list", $data);
+	}
+	/**
+	 * 角色添加页
+	 */
+	public function role_add()
+	{
 		$data = array();
 		$typelist = $this->role->gettypelist();
 		$data['typelist'] = $typelist;
-        $this->display("role/role_add",$data);
-    }
-    /**
-     * 角色添加提交
-     */
-    public function role_save()
-    {
-        if (empty($_SESSION['user_name'])) {
-            echo json_encode(array('error' => false, 'msg' => "无法添加数据"));
-            return;
-        }
+		$this->display("role/role_add",$data);
+	}
+	/**
+	 * 角色添加提交
+	 */
+	public function role_save()
+	{
+		if (empty($_SESSION['user_name'])) {
+			echo json_encode(array('error' => false, 'msg' => "无法添加数据"));
+			return;
+		}
 
-        $rname = isset($_POST["rname"]) ? $_POST["rname"] : '';
-        $rdetails = isset($_POST["rdetails"]) ? $_POST["rdetails"] : '';
+		$rname = isset($_POST["rname"]) ? $_POST["rname"] : '';
+		$rdetails = isset($_POST["rdetails"]) ? $_POST["rdetails"] : '';
 		$menu = isset($_POST["menu"]) ? $_POST["menu"] : '';
 		$menu1 = isset($_POST["menu1"]) ? $_POST["menu1"] : '';
 		if (in_array(3,$menu)){
@@ -65,74 +65,83 @@ class Role extends CI_Controller
 				return;
 			}
 		}
-        $add_time = time();
-        if (empty($rname) || empty($rdetails)) {
-            echo json_encode(array('error' => false, 'msg' => "操作失败"));
-            return;
-        }
-        $role_info = $this->role->getroleByname($rname);
-        if (!empty($role_info)) {
-            echo json_encode(array('error' => true, 'msg' => "该角色已经存在。"));
-            return;
-        }
-        $rid = $this->role->role_save($rname, $rdetails, $add_time);
-        if ($rid) {
-        	if (is_array($menu)){
+		$add_time = time();
+		if (empty($rname) || empty($rdetails)) {
+			echo json_encode(array('error' => false, 'msg' => "操作失败"));
+			return;
+		}
+		$role_info = $this->role->getroleByname($rname);
+		if (!empty($role_info)) {
+			echo json_encode(array('error' => true, 'msg' => "该角色已经存在。"));
+			return;
+		}
+		$rid = $this->role->role_save($rname, $rdetails, $add_time);
+		if ($rid) {
+			if (is_array($menu)){
 				foreach ($menu as $k=>$v){
 					$this->role->rtom_save($rid,$v);
 				}
 			}
-        	if (is_array($menu1)){
+			if (is_array($menu1)){
 				foreach ($menu1 as $k=>$v){
 					$this->role->rtom_save1($rid,$v);
 				}
 			}
-            echo json_encode(array('success' => true, 'msg' => "操作成功。"));
-            return;
-        } else {
-            echo json_encode(array('error' => false, 'msg' => "操作失败"));
-            return;
-        }
-    }
-    /**
-     * 角色删除
-     */
-    public function role_delete()
-    {
-        $id = isset($_POST['id']) ? $_POST['id'] : 0;
+			echo json_encode(array('success' => true, 'msg' => "操作成功。"));
+			return;
+		} else {
+			echo json_encode(array('error' => false, 'msg' => "操作失败"));
+			return;
+		}
+	}
+	/**
+	 * 角色删除
+	 */
+	public function role_delete()
+	{
+		$id = isset($_POST['id']) ? $_POST['id'] : 0;
 		$member_info = $this->role->getmemberById($id);
 		if (!empty($member_info)) {
 			echo json_encode(array('error' => true, 'msg' => "当前角色已有账户设定中。"));
 			return;
 		}
-        if ($this->role->role_delete($id)) {
+		if ($this->role->role_delete($id)) {
 			$this->role->role_delete_rtom($id);
-            echo json_encode(array('success' => true, 'msg' => "删除成功"));
-            return;
-        } else {
-            echo json_encode(array('success' => false, 'msg' => "删除失败"));
-            return;
-        }
-    }
-    /**
-     * 角色修改页
-     */
-    public function role_edit()
-    {
-        $rid = isset($_GET['rid']) ? $_GET['rid'] : 0;
-        $role_info = $this->role->getroleById($rid);
-        if (empty($role_info)) {
-            echo json_encode(array('error' => true, 'msg' => "数据错误"));
-            return;
-        }
-        $data = array();
-        $data['rname'] = $role_info['rname'];
-        $data['rdetails'] = $role_info['rdetails'];
-        $data['rid'] = $rid;
-		$data['role_status1'] = empty($this->role->getroleByIdRtom($rid,1))?0:1;
+			echo json_encode(array('success' => true, 'msg' => "删除成功"));
+			return;
+		} else {
+			echo json_encode(array('success' => false, 'msg' => "删除失败"));
+			return;
+		}
+	}
+	/**
+	 * 角色修改页
+	 */
+	public function role_edit()
+	{
+		$rid = isset($_GET['rid']) ? $_GET['rid'] : 0;
+		$role_info = $this->role->getroleById($rid);
+		if (empty($role_info)) {
+			echo json_encode(array('error' => true, 'msg' => "数据错误"));
+			return;
+		}
+		$data = array();
+		$data['rname'] = $role_info['rname'];
+		$data['rdetails'] = $role_info['rdetails'];
+		$data['rid'] = $rid;
+		/*$data['role_status1'] = empty($this->role->getroleByIdRtom($rid,1))?0:1;
 		$data['role_status2'] = empty($this->role->getroleByIdRtom($rid,2))?0:1;
 		$data['role_status3'] = empty($this->role->getroleByIdRtom($rid,3))?0:1;
+		$typelistme = $this->role->gettypelistme($rid);*/
+
+		$rid_result1 = $this->role->getroleByIdRtom($rid,1);
+		$data['role_status1'] = empty($rid_result1)?0:1;
+		$rid_result2 = $this->role->getroleByIdRtom($rid,2);
+		$data['role_status2'] = empty($rid_result2)?0:1;
+		$rid_result3 = $this->role->getroleByIdRtom($rid,3);
+		$data['role_status3'] = empty($rid_result3)?0:1;
 		$typelistme = $this->role->gettypelistme($rid);
+
 		$data['typelistme'] = $typelistme;
 		$typelist = $this->role->gettypelist();
 		if (!empty($typelistme)){
@@ -145,13 +154,13 @@ class Role extends CI_Controller
 			}
 		}
 		$data['typelist'] = $typelist;
-        $this->display("role/role_edit", $data);
-    }
-    /**
-     * 角色修改提交
-     */
-    public function role_save_edit()
-    {
+		$this->display("role/role_edit", $data);
+	}
+	/**
+	 * 角色修改提交
+	 */
+	public function role_save_edit()
+	{
 		if (empty($_SESSION['user_name'])) {
 			echo json_encode(array('error' => false, 'msg' => "无法添加数据"));
 			return;
@@ -192,6 +201,6 @@ class Role extends CI_Controller
 			return;
 		}
 
-    }
+	}
 
 }
